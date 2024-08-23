a29014c40c4591f61978cb8e5b0a695622297828
FnnyJsst <fanny.jousset@gmail.com>
2024-08-23 07:37:35 +0200

    worked on front-end on the drawer label

--- components/drawers/DrawerLabel.js
+++ components/drawers/DrawerLabel.js
@@ -1,50 +1,76 @@
+import { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const DrawerLabel = ({ label, iconName, iconColor, iconSize, onMoveUp, onMoveDown, onEdit, onDelete }) => (
-  <View style={styles.container}>
-    <Text style={styles.label}>{label}</Text>
-    {label !== "No Channel" && (
-      <>
-        <View style={styles.arrowContainer}>
-          <TouchableOpacity onPress={onMoveUp}>
-            <AntDesign name="up" size={20} color="black" style={styles.up} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onMoveDown}>
-            <AntDesign name="down" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.iconsContainer}>
-          <TouchableOpacity onPress={onEdit}>
-            <EvilIcons name="pencil" size={25} color="black" style={styles.pencil}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onDelete}>
-            <Ionicons name="trash-outline" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
-      </>
-    )}
-  </View>
-);
+const DrawerLabel = ({ label, iconName, iconColor, iconSize, onMoveUp, onMoveDown, onEdit, onDelete }) => {
+  const [upColor, setUpColor] = useState('black');
+  const [downColor, setDownColor] = useState('black');
+  const [pencilColor, setPencilColor] = useState('black');
+  const [binColor, setBinColor] = useState('black');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      {label !== "No Channel" && (
+        <>
+          <View style={styles.arrowContainer}>
+            <TouchableOpacity 
+              onPress={onMoveUp}
+              onPressIn={() => setUpColor('#ff4500')}
+              onPressOut={() => setUpColor('black')}
+            >
+              <AntDesign name="up" size={30} color={upColor} style={styles.up} />
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={onMoveDown}
+              onPressIn={() => setDownColor('#ff4500')}
+              onPressOut={() => setDownColor('black')}
+            >
+              <AntDesign name="down" size={30} color={downColor} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.iconsContainer}>
+            <TouchableOpacity 
+              onPress={onEdit}
+              onPressIn={() => setPencilColor('#ff4500')}
+              onPressOut={() => setPencilColor('black')}
+            >
+              <EvilIcons name="pencil" size={40} color={pencilColor} style={styles.pencil}/>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={onDelete}
+              onPressIn={() => setBinColor('#ff4500')}
+              onPressOut={() => setBinColor('black')}
+            >
+              <Ionicons name="trash-outline" size={30} color={binColor} />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
+    height: 50,
   },
   label: {
     marginLeft: 15, 
     color: 'black',
-    fontSize: 18,
+    fontSize: 20,
   },
   arrowContainer: {
     flexDirection: 'row',
     marginLeft: 'auto',
+    marginRight: 30,
   },
   up: {
-    marginRight: 10,
+    marginRight: 25,
   },
   iconsContainer: {
     flexDirection: 'row',
